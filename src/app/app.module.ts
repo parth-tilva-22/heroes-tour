fa59b19bd60833484619f15a310a04eccbf3517e
MRISOFTWARE\Parth.Tilva <Parth.Tilva@mrisoftware.com>
2022-08-29 12:09:15 +0530

    till 4. add service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,20 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { FormsModule } from '@angular/forms';  // <<<< import it here
 import { AppComponent } from './app.component';
+import { HeroesComponent } from './heroes/heroes.component';
+import { HeroDetailComponent } from './hero-detail/hero-detail.component';
+import { MessagesComponent } from './messages/messages.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeroesComponent,
+    HeroDetailComponent,
+    MessagesComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,FormsModule // <<<< And here
   ],
   providers: [],
   bootstrap: [AppComponent]
